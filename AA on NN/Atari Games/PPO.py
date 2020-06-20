fa0b968664e0a0e4407819b23cfc5a7a42676294
Yifan <xyf.xu@mail.utoronto.ca>
2020-06-19 23:02:37 -0400

    first commit

---
+++ AA on NN/Atari Games/PPO.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jun 12 17:41:49 2020
+
+@author: yifanxu
+"""
+
+import random
+import gym
+import numpy as np
+from PIL import Image
+import torch
+from torch.nn import functional as F
+from torch import nn
+
+class Policy(nn.Module):
+    def __init__(self):
+        super(Policy, self).__init__()
+
+        self.gamma = 0.99
+        self.eps_clip = 0.1
+
+        self.layers = nn.Sequential(
+            nn.Linear(6000, 512), nn.ReLU(),
+            nn.Linear(512, 2),
+        )
+
+    def state_to_tensor(self, I):
+        """ prepro 210x160x3 uint8 frame into 6000 (75x80) 1D float vector. See Karpathy's post: http://karpathy.github.io/2016/05/31/rl/ """
+        if I is None:
+            return torch.zeros(1, 6000)
+        I = I[35:185] # crop - remove 35px from start & 25px from end of image in x, to reduce redundant parts of image (i.e. after ball passes paddle)
+        I = I[::2,::2,0] # downsample by factor of 2.
+        I[I == 144] = 0 # erase background (background type 1)
+        I[I == 109] = 0 # erase background (background type 2)
+        I[I != 0] = 1 # everything else (paddles, ball) just set to 1. this makes the image grayscale effectively
+        return torch.from_numpy(I.astype(np.float32).ravel()).unsqueeze(0)
+
+    def pre_process(self, x, prev_x):
+        return self.state_to_tensor(x) - self.state_to_tensor(prev_x)
+
+    def convert_action(self, action):
+        return action + 2
+
+    def forward(self, d_obs, action=None, action_prob=None, advantage=None, deterministic=False):
+        if action is None:
+            with torch.no_grad():
+                logits = self.layers(d_obs)
+                if deterministic:
+                    action = int(torch.argmax(logits[0]).detach().cpu().numpy())
+                    action_prob = 1.0
+                else:
+                    c = tor
